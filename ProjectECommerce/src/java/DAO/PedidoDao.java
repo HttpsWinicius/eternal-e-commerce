4bb6e02e6a1fad1ba0a49f103059d895298280be
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2020-11-18 22:57:14 -0300

    Padrao Builder no PedidoDao

--- ProjectECommerce/src/java/DAO/PedidoDao.java
+++ ProjectECommerce/src/java/DAO/PedidoDao.java
@@ -6,10 +6,54 @@
 
 package DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import model.Pedido;
+import model.Produto;
+
 /**
  *
  * @author luiz
  */
-public class PedidoDao {
+public class PedidoDao extends DAO{
     
+    private static PedidoDao instancia = new PedidoDao();
+    private Statement stmt;
+
+    private PedidoDao() {}
+
+    public static PedidoDao getInstancia() {
+        return instancia;
+    }
+    
+    public void gravarProduto(Pedido pedido) throws ClassNotFoundException, SQLException{
+        Connection conexao = null;
+        PreparedStatement stmt = null;
+        
+        try{
+            conexao = BD.getInstancia().getConexao();
+            stmt = conexao.prepareStatement("INSERT INTO pedido (id, id_item_pedido, precototal) VALUES (?,?,?);"); 
+            stmt.setInt(1, pedido.getId());
+            stmt.setInt(2, pedido.getItemPedido().getId());
+            stmt.setFloat(3, pedido.getPrecoTotal());
+          
+            stmt.executeUpdate();
+        }finally{
+            fecharConexao(conexao, stmt);
+        }
+    }
+    
+    public Produto instanciarProduto(ResultSet rs) throws SQLException {
+        
+        Produto produto = new Produto();
+        produto.setDescricao(rs.getString("descricao"))
+                .setPreco(rs.getFloat("preco"))
+                .setStatus(rs.getString("status"))
+                .setId(rs.getInt("id"));
+        
+        return produto;
+    }
 }
